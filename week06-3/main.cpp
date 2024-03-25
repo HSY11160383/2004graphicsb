5027d74e19cc9a658e9b03f48902d2507d08845f
HSY <112918655+HSY11160383@users.noreply.github.com>
2024-03-25 16:42:27 +0800

    Add files via upload

---
+++ week06-3/main.cpp
@@ -0,0 +1,13 @@
+#include <GL/glut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glutSoildTeapot(0.3);
+    glutSwapBuffer
+}
+
+int main(int argc,char*argv[])
+{
+    glutInit(&argc, argv);
+
+}
